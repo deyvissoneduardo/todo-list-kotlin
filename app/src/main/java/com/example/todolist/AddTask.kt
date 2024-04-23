931fff222b3c24d9d8b53bdeb50d16f028769f32
Deyvisson Eduardo <deyvissoneduardo22@gmail.com>
2024-04-23 14:32:43 -0300

    layout app

--- app/src/main/java/com/example/todolist/AddTask.kt
+++ app/src/main/java/com/example/todolist/AddTask.kt
@@ -1,22 +1,19 @@
 package com.example.todolist
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.todolist.databinding.ActivityAddTaskBinding
 import com.example.todolist.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class AddTask : AppCompatActivity() {
 
     private val binding by lazy {
-        ActivityMainBinding.inflate(layoutInflater)
+        ActivityAddTaskBinding.inflate(layoutInflater)
     }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
-        binding.fabAdicionar.setOnClickListener{
-            val intent = Intent(this, AddTask::class.java)
-            startActivity(intent)
-        }
+
     }
 }
