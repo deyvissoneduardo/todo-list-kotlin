68058f048a42b5070acc5461f81ce8c5a39338b2
Deyvisson Eduardo <deyvissoneduardo22@gmail.com>
2024-05-06 14:11:14 -0300

    salving and listing tasks

--- app/src/main/java/com/example/todolist/activities/MainActivity.kt
+++ app/src/main/java/com/example/todolist/activities/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.todolist.adapter.TaskAdapter
 import com.example.todolist.database.TaskDAO
 import com.example.todolist.databinding.ActivityMainBinding
 import com.example.todolist.model.TaskModel
@@ -15,6 +17,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private var listTask = emptyList<TaskModel>()
+    private var taskAdapter: TaskAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -24,15 +27,19 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
+        taskAdapter = TaskAdapter()
+        binding.rvTarefas.adapter = taskAdapter
+        binding.rvTarefas.layoutManager = LinearLayoutManager(this)
     }
 
     override fun onStart() {
         super.onStart()
+        loadNewTask()
+    }
+
+    private fun loadNewTask(){
         val taskDAO = TaskDAO(this)
         listTask = taskDAO.getAll()
-
-        listTask.forEach{task ->
-            Log.i("info__db", "Task ${task.description.toString()} \n")
-        }
+        taskAdapter?.loadTask(listTask)
     }
 }
---
+++ app/src/main/java/com/example/todolist/adapter/TaskAdapter.kt
@@ -0,0 +1,49 @@
+package com.example.todolist.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todolist.databinding.ItemTaskBinding
+import com.example.todolist.model.TaskModel
+
+class TaskAdapter():  RecyclerView.Adapter<TaskAdapter.TaskViewHolder>() {
+
+    private var listTask: List<TaskModel> = emptyList()
+
+    inner class TaskViewHolder(itemBinding: ItemTaskBinding)
+        : RecyclerView.ViewHolder(itemBinding.root) {
+
+        private val binding: ItemTaskBinding
+
+        init {
+            binding = itemBinding
+        }
+
+        fun bind(taskmodel: TaskModel){
+            binding.textDescricao.text = taskmodel.description
+            binding.textData.text = taskmodel.date
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        val itemTarefaBinding = ItemTaskBinding.inflate(
+            LayoutInflater.from(parent.context), parent, false
+        )
+        return TaskViewHolder(itemTarefaBinding)
+    }
+
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        val task = listTask[position]
+        holder.bind(task)
+    }
+
+    override fun getItemCount(): Int {
+        return listTask.size
+    }
+
+    fun loadTask(task: List<TaskModel>){
+        this.listTask = task
+        notifyDataSetChanged()
+    }
+}
